11c21229ca751bfed6b6bb6eb8500e7987599b7d
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2024-10-04 21:11:55 +0900

    dotnet のコンテナを追加した。

---
+++ src/dotnet/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# https://github.com/devcontainers/cli/blob/v0.71.0/src/spec-node/devContainersSpecCLI.ts#L505
+devcontainer build --docker-path podman --workspace-folder . --image-name dotnet-ubi9 --push false
